d16735dc0a84757167329699c33b33727129de33
ludovicm67 <ludovicmuller1@gmail.com>
2017-03-22 09:30:46 +0100

    initial commit

---
+++ detecter.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * @brief Vérifie s'il y a une erreur
+ * @details Vérifie si la commande (qui doit nécéssairement retourner un int,
+ *          avec -1 en cas d'erreur) dans status a échouée ou non, ce qui est
+ *          très pratique pour les primitives système par exemple. Lors d'une
+ *          erreur, on affiche le message 'msg' et on quitte le programme.
+ *
+ * @param status un entier, en général le code de retour d'une fonction
+ * @param msg le message à afficher en cas d'erreur
+ */
+int check_error(int status, char * msg) {
+    if (status == -1) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+    return status;
+}
+
+int main(int argc, char *argv[]) {
+	int c, errflg = 0;
+	extern char *optarg;
+	extern int optind;
+
+	char * time_format = NULL; // option -t
+	int interval = 10000; // option -i
+	int limit = 0; // option -l
+	int code_change = 0; // option -c
+
+	while ((c = getopt(argc, argv, "+t:i:l:c")) != EOF) {
+		switch (c) {
+			case 't':
+				time_format = optarg;
+				break;
+			case 'i':
+				interval = atoi(optarg);
+				break;
+			case 'l':
+				limit = atoi(optarg);
+				break;
+			case 'c':
+				code_change = 1;
+				break;
+			case '?': /*option non reconnue */
+				errflg++;
+				break;
+		}
+	}
+
+	printf("OPTION t = %s\n", time_format);
+	printf("OPTION i = %d\n", interval);
+	printf("OPTION l = %d\n", limit);
+	if (code_change) printf("OPTION c :)\n");
+
+	(void) time_format;
+	(void) interval;
+	(void) limit;
+	(void) code_change;
+
+	return EXIT_SUCCESS;
+}
