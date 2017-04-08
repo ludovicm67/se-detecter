feb473e5d08a0a73b7265f293b2f70d67a5b2a95
E-stein <eloise.stein@outlook.fr>
2017-04-08 17:21:58 +0200

    delete detecter.h

--- detecter.c
+++ detecter.c
@@ -5,9 +5,61 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include "detecter.h"
 
-// Initialise un nouveau buffer
+#define BUFFER_SIZE 1024
+
+// Affiche un message sur la sortie d'erreur standard si status == value
+// et quitte le programme avec un EXIT_FAILURE
+#define CHECK_ERRVALUE(status, value, msg, b)           \
+    if(status == value) {                               \
+        fprintf(stderr, "%s\n", msg);                   \
+        free_buffer(b);                                 \
+        exit(EXIT_FAILURE);                             \
+    }
+
+// Affiche un message sur la sortie d'erreur standard si status == NULL,
+// et quitte le programme avec un EXIT_FAILURE
+#define CHECK_NULL(status, msg, b) CHECK_ERRVALUE(status, NULL, msg, b)
+
+// Affiche un perror et quitte le programme avec un EXIT_FAILURE si status==-1
+#define CHECK_ERR(status, msg, b)                       \
+    if(status == -1) {                                  \
+        perror(msg);                                    \
+        free_buffer(b);                                 \
+        exit(EXIT_FAILURE);                             \
+    }
+
+// Gère les erreurs après un exec
+#define CHECK_EXEC(b)                                   \
+    perror("execvp");                                   \
+    free_buffer(b);                                     \
+    CHECK_ERR(kill(getpid(), SIGUSR1), "kill", NULL);   \
+    exit(EXIT_FAILURE)
+
+// Structure pour un buffer chaîné
+typedef struct buffer {
+    char content[BUFFER_SIZE]; // contenu du buffer
+    struct buffer* next; // pointeur sur le buffer suivant
+    unsigned int size;
+} * Buffer;
+
+/**
+ * @brief Libère la mémoire utilisée par le buffer
+ * @details Libère la mémoire utilisée par une suite de buffer chaînées
+ *
+ * @param b Le buffer que l'on ne souhaite plus utiliser
+ */
+void free_buffer(Buffer b) {
+    if(!b) return;
+    if(b->next) free_buffer(b->next);
+    free(b);
+}
+
+/**
+ * @brief Initialise un nouveau buffer
+ * @details Alloue un nouveau buffer et l'initialise
+ * @return Le nouveau buffer
+ */
 Buffer new_buffer() {
     Buffer b = malloc(sizeof(struct buffer));
     CHECK_NULL(b, "buffer non alloué", b);
@@ -16,14 +68,13 @@ Buffer new_buffer() {
     return b;
 }
 
-// Libère la mémoire utilisée par le buffer chaîné
-void free_buffer(Buffer b) {
-    if(!b) return;
-    if(b->next) free_buffer(b->next);
-    free(b);
-}
-
-// Affiche le contenu stocké dans les buffer chaînés
+/**
+ * @brief Affiche le contenu du buffer
+ * @details Affiche le contenu stocké dans les buffer chaînés sur la sortie
+ *          standard
+ *
+ * @param b Le buffer à afficher
+ */
 void print_buffer(Buffer b) {
     if(!b) return;
     for(; b->next != NULL; b = b->next) {
@@ -31,7 +82,15 @@ void print_buffer(Buffer b) {
     }
 }
 
-// Lit le contenu de fd et l'inscrit dans le buffer b
+/**
+ * @brief Rmplit le buffer b avec le ceontenu de fd
+ * @details Lit le contenu de fd et l'inscrit dans le buffer b
+ *
+ * @param fd Descripteur de fichier où on doit effectuer la lecture
+ * @param b Buffer dans lequel on doit écrire
+ *
+ * @return 1 si le contenu du buffer a changé, 0 sinon
+ */
 unsigned int read_buffer(int fd, Buffer b) {
     unsigned int has_changed = 0, size;
     char tmp[BUFFER_SIZE];
@@ -50,7 +109,12 @@ unsigned int read_buffer(int fd, Buffer b) {
     return has_changed;
 }
 
-// Affiche la manière dont on doit utiliser le programme
+/**
+ * @brief Affiche la manière dont on doit utiliser le programme
+ * @details Affiche les différentes options et arguments pour ce programme
+ *
+ * @param program_name Le nom du programme
+ */
 void usage(char * program_name) {
     fprintf(stderr, "Usage: %s ", program_name);
     fprintf(stderr, "[-t format][-i intervalle][-l limite][-c] ");
@@ -59,7 +123,13 @@ void usage(char * program_name) {
     exit(EXIT_FAILURE);
 }
 
-// Affiche le temps actuel dans le format time_format spécifié
+/**
+ * @brief Affiche le temps courant
+ * @details Affiche le temps actuel dans le format time_format spécifié
+ *
+ * @param time_format Format dans lequel on souhaite afficher le temps
+ * @param b Le buffer (pour le free en cas d'erreur)
+ */
 void print_time(char * time_format, Buffer b) {
     char outstr[200];
     struct tm *tmp;
@@ -121,7 +191,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // S'il y a eu un problème d'options ou d'argument
     if(errflg) usage(argv[0]);
     nb_args = argc - optind;
     if(nb_args <= 0) usage(argv[0]);
